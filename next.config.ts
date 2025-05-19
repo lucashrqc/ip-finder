5eaddeae637a56e89dda8dc5f68772c648ed3dc0
lucashrqc <lucas.hrqc@gmail.com>
2025-05-19 18:45:34 -0300

    fix: next js config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/ip-finder",
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
